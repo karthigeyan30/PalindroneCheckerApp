9129920d48e7b2858b6ff0f0bf5d9a02ef6d3f78
karthigeyan30 <ks4156@srmist.edu.in>
2026-02-26 09:44:56 +0530

    [karthi] added usecaseName

---
+++ App/src/PalindroneCheckerApp.java
@@ -0,0 +1,51 @@
+/**
+ * =========================================================
+ * Palindrome Checker Management System
+ * =========================================================
+ *
+ * UC1: Application Entry & Welcome Message
+ *
+ * Objective:
+ * Display application details at startup before
+ * implementing palindrome validation logic.
+ *
+ * Key Concepts:
+ * - Class
+ * - main() method
+ * - static keyword
+ * - Console output
+ * - Basic application flow control
+ *
+ * @version 1.0
+ */
+
+public class PalindromeCheckerApp {
+
+    /**
+     * Entry point of the Java application.
+     * JVM starts execution from this method.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // Display Welcome Message
+        System.out.println("==============================================");
+        System.out.println("   Welcome to Palindrome Checker App");
+        System.out.println("==============================================");
+
+        // Display Application Details
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version          : 1.0");
+        System.out.println("Status           : System Initialized Successfully");
+
+        System.out.println("==============================================");
+
+
+
+    }
+}
+
+
+
+
